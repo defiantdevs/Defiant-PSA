4336e7ebdffebf4e73c2e0bd198fe21ee0b293ba
defiantdevs <155750978+defiantdevs@users.noreply.github.com>
2025-01-13 10:29:46 -0600

    Update side_nav.php

--- side_nav.php
+++ side_nav.php
@@ -2,8 +2,8 @@ IAT<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-<?php echo nullable_htmlentities($config_theme); ?> d-print-none">
 
         <a class="brand-link" href="dashboard.php">
-     <div class="brand-image">
-    <img alt="<?=nullable_htmlentities($company_name)?> logo" class="img-fluid" style="max-width: 100%; height: auto;" src="<?php echo !empty($company_logo) ? "./uploads/settings/$company_logo" : "./uploads/settings/wwt-white.png"; ?>">
+     <div class="brand-image" style="height: 100%;">
+    <img alt="<?=nullable_htmlentities($company_name)?> logo" class="img-fluid" style="max-width: 100%; height: 100%;" src="<?php echo !empty($company_logo) ? "./uploads/settings/$company_logo" : "./uploads/settings/wwt-white.png"; ?>">
 </div>
     </a>
     
